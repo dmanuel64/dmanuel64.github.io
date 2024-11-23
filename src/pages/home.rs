c848f7e2705f8af643aa21c5fc7dea817930766d
Dylan Manuel <dylan.manuel@att.net>
2024-11-23 16:41:38 -0600

    added tailwindcss

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -2,5 +2,22 @@ use leptos::*;
 
 #[component]
 pub fn Home() -> impl IntoView {
-    view! {<p>"Hello world"</p>}
+    let (count, set_count) = create_signal(0);
+
+    view! {
+        <main class="my-0 mx-auto max-w-3xl text-center">
+            <h2 class="p-6 text-4xl">"Welcome to my Website"</h2>
+            <p class="px-10 pb-10 text-center">"Hello, world"</p>
+            <button
+                class="bg-sky-600 hover:bg-sky-700 px-5 py-3 text-white rounded-lg"
+                on:click=move |_| set_count.update(|count| *count += 1)
+            >
+                {move || if count() == 0 {
+                    "Click me!".to_string()
+                } else {
+                    count().to_string()
+                }}
+            </button>
+        </main>
+    }
 }
