7ca025a6722eb8d43b8754005142d106221660bc
Dylan Manuel <dylan.manuel@att.net>
2024-11-23 20:05:42 -0600

    added thaw UI

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -1,23 +1,24 @@
 use leptos::*;
+use thaw::*;
 
 #[component]
 pub fn Home() -> impl IntoView {
     let (count, set_count) = create_signal(0);
 
     view! {
-        <main class="my-0 mx-auto max-w-3xl text-center animate-fadeIn">
-            <h2 class="p-6 text-4xl">"Welcome to my Website"</h2>
-            <p class="px-10 pb-10 text-center">"Hello, world"</p>
-            <button
-                class="bg-sky-600 hover:bg-sky-700 px-5 py-3 text-white rounded-lg"
-                on:click=move |_| set_count.update(|count| *count += 1)
-            >
-                {move || if count() == 0 {
-                    "Click me!".to_string()
-                } else {
-                    count().to_string()
-                }}
-            </button>
-        </main>
+        <Layout class="my-0 mx-auto max-w-3xl text-center animate-fadeIn">
+            <LayoutHeader class="p-6 text-4xl">"Welcome to my Website"</LayoutHeader>
+                <Text class="block px-10 pb-10 text-center">"Hello, world"</Text>
+                <Button
+                    class="block mx-auto bg-sky-600 hover:bg-sky-700 px-5 py-3 text-white rounded-lg"
+                    on:click=move |_| set_count.update(|count| *count += 1)
+                >
+                    {move || if count() == 0 {
+                        "Click me!".to_string()
+                    } else {
+                        count().to_string()
+                    }}
+                </Button>
+        </Layout>
     }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
+  darkMode: 'class',
   content: {
     files: ["*.html", "./src/**/*.rs"],
   },
