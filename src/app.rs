4924acdb8eadf02f2c7a311943b33a995949e7a1
Dylan Manuel <dylan.manuel@att.net>
2025-06-27 21:44:31 -0500

    chore: downgraded leptos to 7.8 and associated libraries to work with thaw 0.4

--- src/app.rs
+++ src/app.rs
@@ -1,22 +1,33 @@
 use leptos::prelude::*;
 use leptos_router::{components::*, path};
+use thaw::*;
 
 use crate::pages::*;
 
+#[component]
+fn NavBar() -> impl IntoView {
+    view! {
+        <Flex>
+            <Text>"Hello"</Text>
+        </Flex>
+    }
+}
 #[component]
 pub fn App() -> impl IntoView {
     view! {
         <div id="root">
             <Router>
-                <nav></nav>
+                <nav>
+                    <NavBar />
+                </nav>
                 <main>
-                    // all our routes will appear inside <main>
                     <Routes fallback=|| view! { <h1>"Not Found"</h1> }>
                         <Route path=path!("/") view=Home />
-                        // <Route path=path!("/blog") view=Users />
-                        // <Route path=path!("/blog/:id") view=UserProfile />
+                    // <Route path=path!("/blog") view=Users />
+                    // <Route path=path!("/blog/:id") view=UserProfile />
                     </Routes>
                 </main>
+                <footer></footer>
             </Router>
         </div>
     }
